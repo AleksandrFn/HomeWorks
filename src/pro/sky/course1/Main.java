101d3ad6103d9645fdad4a68c5f2871e595757ce
AleksandrFn <godldamn@mail.ru>
2022-09-19 14:18:39 +0300

    ДЗ2

--- src/pro/sky/course1/Main.java
+++ src/pro/sky/course1/Main.java
@@ -2,23 +2,20 @@ package pro.sky.course1;
 
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println("Dog = "+dog);
-        System.out.println("Cat = "+cat);
-        System.out.println("Peper = "+paper);
-        dog = dog+4;
-        cat = cat+4;
-        paper = paper+4;
-        System.out.println("Dog = "+dog);
-        System.out.println("Cat = "+cat);
-        System.out.println("Peper = "+paper);
-        dog -= 3.5;
-        cat -= 1.6;
-        paper -= 7639;
-        System.out.println("Dog = "+dog);
-        System.out.println("Cat = "+cat);
-        System.out.println("Peper = "+paper);
+        var fried =19;
+        System.out.println(fried);
+        fried += 2;
+        System.out.println(fried);
+        fried /= 7;
+        System.out.println(fried);
+
+        var dog = 3.5;
+        System.out.println(dog);
+        dog *= 10;
+        System.out.println(dog);
+        dog /= 3.5;
+        System.out.println(dog);
+        dog += 4;
+        System.out.println(dog);
     }
 }
