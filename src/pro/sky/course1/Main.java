d0d1a5c1ef2c7e0c5cd1bfc819a4849720b3116d
AleksandrFn <godldamn@mail.ru>
2022-09-19 14:08:01 +0300

    Test

--- src/pro/sky/course1/Main.java
+++ src/pro/sky/course1/Main.java
@@ -2,6 +2,23 @@ package pro.sky.course1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("Dog = "+dog);
+        System.out.println("Cat = "+cat);
+        System.out.println("Peper = "+paper);
+        dog = dog+4;
+        cat = cat+4;
+        paper = paper+4;
+        System.out.println("Dog = "+dog);
+        System.out.println("Cat = "+cat);
+        System.out.println("Peper = "+paper);
+        dog -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+        System.out.println("Dog = "+dog);
+        System.out.println("Cat = "+cat);
+        System.out.println("Peper = "+paper);
     }
 }
